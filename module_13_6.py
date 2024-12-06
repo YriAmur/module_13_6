d20268a17cb6832453769cce6bcaf9be3057fb4d
YriAmur <yr1979a@gmail.com>
2024-12-06 23:02:15 +0900

    Update module_13_6.py

--- module_13_6.py
+++ module_13_6.py
@@ -11,25 +11,29 @@ api = ""
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
-
 # Определение состояний
 class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
 
-kb = ReplyKeyboardMarkup(resize_keyboard=True)
-button = KeyboardButton(text='Рассчитать')
-button1 = KeyboardButton(text='Информация')
-kb.add(button)
-kb.add(button1)
+kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Рассчитать'),
+            KeyboardButton(text='Информация')
+        ]
+    ], resize_keyboard=True
+)
 
-kb_inl = InlineKeyboardMarkup(resize_keyboard=True)
-button3= InlineKeyboardButton('Рассчитать норму калорий', callback_data='calories')
-button4 = InlineKeyboardButton('Формулы расчёта', callback_data='formulas')
-kb_inl.add(button3)
-kb_inl.add(button4)
-
+kb_inl = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton('Рассчитать норму калорий', callback_data='calories'),
+            InlineKeyboardButton('Формулы расчёта', callback_data='formulas')
+        ]
+    ], resize_keyboard=True
+)
 
 # Функция /start
 @dp.message_handler(commands=['start'])
@@ -40,6 +44,11 @@ async def start(message: types.Message):
 @dp.message_handler(text=['Рассчитать'])
 async def main_menu(message: types.Message):
     await message.answer('Выберите опцию:', reply_markup=kb_inl)
+
+# Функция для обработки нажатия кнопки 'Информация'
+@dp.message_handler(text=['Информация'])
+async def info(message: types.Message):
+    await message.answer('Данный бот рассчитает суточную норму потребления калорий')
 
 # Функция для получения формул
 @dp.callback_query_handler(lambda call: call.data == 'formulas')
@@ -64,7 +73,6 @@ async def set_growth(message: types.Message, state: FSMContext):
     await message.answer('Введите свой рост:')
     await UserState.growth.set()  # Переход к состоянию growth
 
-
 # Функция для установки веса
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message: types.Message, state: FSMContext):
@@ -72,15 +80,13 @@ async def set_weight(message: types.Message, state: FSMContext):
     await message.answer('Введите свой вес:')
     await UserState.weight.set()  # Переход к состоянию weight
 
-
 # Функция для отправки калорий
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message: types.Message, state: FSMContext):
     await state.update_data(weight=message.text)  # Сохранение веса
 
-
-
-    data = await state.get_data()  # Получение всех введенных данных
+# Получение всех введенных данных
+    data = await state.get_data()  
     age = int(data.get('age'))
     growth = int(data.get('growth'))
     weight = int(data.get('weight'))
